dd37fa6d79e801b007b24f7f3fda0ff1738f3262
EIZOUKI ISSAM <issam.eizouki@etu.unistra.fr>
2021-10-24 23:49:49 +0200

    server.c

--- server.c
+++ server.c
@@ -29,26 +29,47 @@ int etablissementConnexion (int s,int ipDistante,int portLocal,int portEcoute,st
 
     char buf[DEFAULTSIZE];
     memset(buf, '\0',DEFAULTSIZE);
-    
+
+
+
+    fd_set fd_monitor;
+    struct timeval tv;
+    int retval;
+
+    FD_ZERO(&fd_monitor);
+    FD_SET(s, &fd_monitor);
+
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+
 //on peut ajouter une section de test temporaire pour tester si nos adresse et port sont justes
     while(1){
+
+    retval = select(1, &fd_monitor, NULL, NULL, &tv);
+    if(retval==-1){
+        printf("select etablissement\n");
+    }
+    if(FD_ISSET(s,&fd_monitor)){
+                printf("data ready");//Je receve et je test et si tout va bien je renvois avec les nouvelles valeurs
+
     if((retour=recvfrom(s,buf,DEFAULTSIZE,0,(struct sockaddr*)&client,&size))==-1){
         raler("recvfrom");
     }
 
     char*inter_buf = buf;
     //analyse de données reçu :
-
+    
 
     //preparation de donnée (ACK à envoyer)
 
 
     //envoie d'ACK
 
-
+    
     //Attendre la confirmation de reçu de l'ACK de la part de source 
 
-
+    }
     //Connexion établie
     printf("connexion établie\n");
 
