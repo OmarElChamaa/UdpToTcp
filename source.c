1d369ddf4e6394a8c5126b1706af15955bd78888
EL-CHAMAA OMAR <chamaael@etu.unistra.fr>
2021-10-28 12:14:22 +0200

    optimisation etablissement

--- source.c
+++ source.c
@@ -19,154 +19,164 @@
 #include <string.h>
 #include <sys/select.h>
 
-// int id =0;
+int id =0;
 
-// int etablissementConnexion(int s,int ipDistante,int portLocal,
-// int portEcoute,struct sockaddr_in ecoute,struct sockaddr_in envoie)
-// {
+int etablissementConnexion(int s,struct sockaddr_in ecoute,
+struct sockaddr_in envoie)
+{
 
-//     int a = generateRandInt(5000);
-//     char buff [DEFAULTSIZE] ;
+    int a = generateRandInt(5000);
+    char buff [DEFAULTSIZE] ;
 
-//     struct packet* p=init_packet() ;
+    struct packet p=init_packet() ;
     
-//     p->id=id++ ;
-//     p->type.SYN=1;
-//     p->seq=a;
+    p.id=id++ ;
+    p.type.SYN=1;
+    p.seq=a;
 
-//     const char * packetToSend=generatePacket(*p);
-
-//     fd_set fd_monitor;
-//     struct timeval tv;
-//     int retval;
-
-//     FD_ZERO(&fd_monitor);
-//     FD_SET(s, &fd_monitor);
-
-//     tv.tv_sec = 5;
-//     tv.tv_usec = 0;
+    char * packetToSend = malloc(sizeof(char)*DEFAULTSIZE) ;
+    memset(packetToSend,'\0',DEFAULTSIZE);
+    generatePacket(p,packetToSend) ;
 
 
-//     while(1){
-//         send_to_establish(s,packetToSend,DEFAULTSIZE,0,(struct sockaddr*)&envoie,
-//         sizeof(portEcoute));
+    fd_set fd_monitor;
+    struct timeval tv;
+    int retval;
 
-//         retval = select(1, &fd_monitor, NULL, NULL, &tv);
-//             if(retval==-1){
-//                 printf("select etablissement\n");
-//             }
+    FD_ZERO(&fd_monitor);
+    FD_SET(s, &fd_monitor);
 
-//         if(FD_ISSET(s,&fd_monitor)){
-//                 printf("data ready");//Je receve et je test et si tout va bien je renvois avec les nouvelles valeurs
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
 
-//                 //recevfrom
-//                 socklen_t size=sizeof(envoie);
-//                 int r =recvfrom(s,buff,DEFAULTSIZE+1,0,(struct sockaddr*)&ecoute,&size);
-//                 if(r==-1){
-//                     raler("recvfrom 1\n");
-//                 }
-//                 //deroulement de test
+
+    while(1){
+        send_to_establish(s,packetToSend,DEFAULTSIZE,0,(struct sockaddr*)&envoie,
+        sizeof(envoie));
+
+        retval = select(1, &fd_monitor, NULL, NULL, &tv);
+            if(retval==-1){
+                printf("select etablissement\n");
+            }
+
+        if(FD_ISSET(s,&fd_monitor)){
+                printf("data ready");//Je receve et je test et si tout va bien je renvois avec les nouvelles valeurs
+
+                //recevfrom
+                socklen_t size=sizeof(envoie);
+                int r =recvfrom(s,buff,DEFAULTSIZE+1,0,(struct sockaddr*)&ecoute,&size);
+                if(r==-1){
+                    raler("recvfrom 1\n");
+                }
+                //deroulement de test
                 
-//                 p=generatePacketFromBuf(buff);//on genere le paquet du buf recu
-//                 if(p->type.ACK==a+1){
-//                     p->acq=p->seq+1;//ack = b+1
-//                     p->id++;//id++
-//                     //envoyer le dernier paquet en confirmant avoir recu l'ack
-//                     send_to_establish(s,packetToSend,DEFAULTSIZE,0,(struct sockaddr * )&envoie,
-//                     sizeof(portEcoute));
+                p=generatePacketFromBuf(buff);//on genere le paquet du buf recu
+                if(p.type.ACK==a+1){
+                    p.acq=p.seq+1;//ack = b+1
+                    p.id++;//id++
+                    //envoyer le dernier paquet en confirmant avoir recu l'ack
+                    send_to_establish(s,packetToSend,DEFAULTSIZE,0,(struct sockaddr * )&envoie,
+                    sizeof(envoie));
                    
-//                 }
-//                 else{
-//                     printf("mauvais comportement du serveur en 3 way-shakehand\n");
-//                     exit(EXIT_FAILURE);
-//                 }
+                }
+                else{
+                    printf("mauvais comportement du serveur en 3 way-shakehand\n");
+                    exit(EXIT_FAILURE);
+                }
 
 
-//                 printf("connexion établie!");
-//                 exit(EXIT_SUCCESS);
-//         }else{
-//             continue;
-//         }
-//     }
-// }
-// /*
-// void goBack(int ipDistante,int portLocal,int portEcoute){
+                printf("connexion établie!");
+                exit(EXIT_SUCCESS);
+        }else{
+            continue;
+        }
+    }
+}
 
-//     //int fenetre = ;
+void goBack(int s,struct sockaddr_in envoie,struct sockaddr_in ecoute){
 
-//     int s = 0; 
-//     creationSocket(s);
+    //int fenetre = ;
 
-//     struct sockaddr_in ecoute ;
-//     struct sockaddr_in envoie ;
-
-//     ecoute.sin_family = AF_INET ;
-//     ecoute.sin_port = htons(portEcoute);
-//     ecoute.sin_addr.s_addr=INADDR_ANY;
-
-//     envoie.sin_family =AF_INET;
-//     envoie.sin_port = htons(portLocal);
-//     envoie.sin_addr.s_addr=INADDR_ANY;
-
-//     binding(s,ecoute);
+    binding(s,ecoute);
 
 
-// }
+}
 
-// void stopNwait(int ipDistante,int portLocal,int portEcoute){
+void stopNwait(int s,struct sockaddr_in envoie,struct sockaddr_in ecoute){
 
-// }
+}
 
 
-// int main(int argc, char * argv[]){
+int main(int argc, char * argv[]){
 
-//     if(argc!=5){
-//         raler("Mauvais nombre d'arguments \n");
-//     }
-//     char *mode=argv[1];
-//     char *ipDistante=argv[2];
-//     char *portLocal=argv[3];
-//     char *portEcoute=argv[4];
+    if(argc!=5){
+        raler("Mauvais nombre d'arguments \n");
+    }
+    char *mode=argv[1];
+    char *ipDistante=argv[2];
+    char *portLocal=argv[3];
+    char *portEcoute=argv[4];
 
-//     int ipDistInt=atoi(ipDistante);
-//     int portLocalInt=atoi(portLocal);
-//     int portEcouteInt=atoi(portEcoute);
-//     int modeInt = atoi(mode); 
+    int ipDistInt=atoi(ipDistante);
+    int portLocalInt=atoi(portLocal);
+    int portEcouteInt=atoi(portEcoute);
+    int modeInt = atoi(mode); 
 
-//     if(modeInt==1){
 
-//         printf("Vous avez choisi le mode stop-wait \n");
-//         stopNwait(ipDistInt,portLocalInt,portEcouteInt);  
+    struct sockaddr_in ecoute ;
+    struct sockaddr_in envoie ;
+
+    ecoute.sin_family = AF_INET;
+    ecoute.sin_port = htons(portLocalInt);
+    ecoute.sin_addr.s_addr = INADDR_ANY ;
+
+    envoie.sin_family = AF_INET;
+    envoie.sin_port = htons(portEcouteInt);
+    envoie.sin_addr.s_addr = INADDR_ANY ;
+
+    int s = 0;
+    s=creationSocket(s);
+
+    if(etablissementConnexion(s,ecoute,envoie)==0){
+        printf("Connexion etablie avec Succes \n ");
+        printf("/******************************/\n");
+    }else{
+        printf("Erreur Lors de L'etablissement de connexion\n");
+        printf("/******************************/\n");
+        return EXIT_FAILURE;
+    }
+
+
+
+    if(modeInt==1){
+
+        printf("Vous avez choisi le mode stop-wait \n");
+        stopNwait(s,envoie,ecoute);  
       
-//     }else{
+    }else{
 
-//         printf("Vous avez choisi le mode go-back-in \n");
-//         goBack(ipDistInt,portLocalInt,portEcouteInt);
-//     }
-//     return 0 ;
-// }
-// */
-// int main(){
-//     return 0;
-// }
-
+        printf("Vous avez choisi le mode go-back-in \n");
+        goBack(s,envoie,ecoute);
+    }
+    return 0 ;
+}
 
 // #include "packet.h"
 
-int main(){
-    char buf[416] = "0000111100000001000000010000000100000001000000010000000100000001";
-    packet p = init_packet(p);
-    p=generatePacketFromBuf(buf);
+// int main(){
+//     char buf[416] = "0000111100000001000000010000000100000001000000010000000100000001";
+//     packet p = init_packet(p);
+//     p=generatePacketFromBuf(buf);
 
-    printf("ID : %d\n",p.id);
-    printf("Type ACK : %d\n",p.type.ACK);
-    printf("Type FIN : %d\n",p.type.FIN);
-    printf("Type RST : %d\n",p.type.RST);
-    printf("Type SYN : %d\n",p.type.SYN);
-    printf("acq : %d\n",p.acq);
-    printf("ECN : %d\n",p.ecn);
-    printf("Fenetre : %d\n",p.fenetre);
-    printf("seq : %d\n",p.seq);
+//     printf("ID : %d\n",p.id);
+//     printf("Type ACK : %d\n",p.type.ACK);
+//     printf("Type FIN : %d\n",p.type.FIN);
+//     printf("Type RST : %d\n",p.type.RST);
+//     printf("Type SYN : %d\n",p.type.SYN);
+//     printf("acq : %d\n",p.acq);
+//     printf("ECN : %d\n",p.ecn);
+//     printf("Fenetre : %d\n",p.fenetre);
+//     printf("seq : %d\n",p.seq);
     
-    return 0;
-}
+//     return 0;
+// }
