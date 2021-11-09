3558617f164e9a12ea1a9dffeb349f9ea75356a6
EL-CHAMAA OMAR <chamaael@etu.unistra.fr>
2021-11-09 21:42:38 +0100

    Commentaires+ changement val retour 0 si bonne exec 1 sinon

--- source.c
+++ source.c
@@ -3,6 +3,14 @@
 
 int id = 0 ;
 
+/**
+    @brief Fonction implementant la procedure stop n wait du cote source  
+    @param socket
+    @param sockaddr_in ecoute
+    @param sockaddr_in envoie
+    @return int 
+*/
+
 int stopNwait(int s,struct sockaddr_in ecoute,
 struct sockaddr_in envoie){
     
@@ -75,7 +83,9 @@ struct sockaddr_in envoie){
                 x=recvfrom(s,&p,DEFAULTSIZE,0,
                 (struct sockaddr*)&ecoute,&size);
 
-                //test fin connexion->4 way handshake ;
+                if(p.type==2){
+                    fermeture_connection_source(s,ecoute,envoie);
+                }
 
                 if(x==-1){
                     if(close(s)==-1){
